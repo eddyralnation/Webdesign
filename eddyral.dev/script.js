2a9fbfc0ca4d5d17935f8689c036c23df557c916
Harishwa <138890307+eddyralnation@users.noreply.github.com>
2024-10-24 22:41:19 +0530

    Second commit
    Text added with bg

--- eddyral.dev/script.js
+++ eddyral.dev/script.js
@@ -1,18 +1,24 @@
 function togglemenu()
 {
-  var menuList=document.getElementById('menuList')
+  var menuList=document.getElementById('menuList');
+  var video=document.querySelector('video');
+  var bodycontent=document.querySelector('body');
 
   if(menuList.style.maxWidth=="0px" )
   {
     menuList.style.maxWidth="500px";
     menuList.style.opacity="100%";
     actionIcon.classList.add("action");
+    video.classList.add("blur");   // Added blur to video
+    bodyContent.classList.add("blur");
   }
   else
   {
     menuList.style.maxWidth="0px";
     menuList.style.opacity="0%";
     actionIcon.classList.remove("action");
+    video.classList.remove("blur");   // Added blur to video
+    bodyContent.classList.remove("blur");
 
   }
 }
