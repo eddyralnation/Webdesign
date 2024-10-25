0c5f5cfbe87ba52e3837c499c1c164a0ae2a9f6c
Harishwa <138890307+eddyralnation@users.noreply.github.com>
2024-10-25 22:08:42 +0530

    3rd commit
    Added further elements to the home page

--- eddyral.dev/script.js
+++ eddyral.dev/script.js
@@ -2,7 +2,7 @@ function togglemenu()
 {
   var menuList=document.getElementById('menuList');
   var video=document.querySelector('video');
-  var bodycontent=document.querySelector('body');
+  var bodycontent=document.querySelector('.heading');
 
   if(menuList.style.maxWidth=="0px" )
   {
@@ -10,15 +10,15 @@ function togglemenu()
     menuList.style.opacity="100%";
     actionIcon.classList.add("action");
     video.classList.add("blur");   // Added blur to video
-    bodyContent.classList.add("blur");
+    bodyContent.classList.add("blur"); //in the making(to be ignored for now)
   }
   else
   {
     menuList.style.maxWidth="0px";
     menuList.style.opacity="0%";
     actionIcon.classList.remove("action");
-    video.classList.remove("blur");   // Added blur to video
-    bodyContent.classList.remove("blur");
+    video.classList.remove("blur");   // removed blur to video
+    bodyContent.classList.remove("blur"); //in the making(to be ignored for now)
 
   }
 }
